4dd75d1cad9dbb409f5741ebb2a66321f7ae7c4b
Godswill Udida <udidagodswill7@gmail.com>
2023-12-08 16:07:41 +0100

    udida

--- AfriHack_Be/controller/userController.ts
+++ AfriHack_Be/controller/userController.ts
@@ -1,89 +1,128 @@
 import { Request, Response } from "express";
 import { HTTP } from "../error/mainError";
 import userModel from "../model/userModel";
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { Role } from "../utils/role";
 
-export const createUser = async (req:Request,res:Response):Promise<Response> => {
-    try {
+export const createUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { userName, email, password } = req.body;
 
-        const { userName, email, password } = req.body;
+    const encrypt = await bcrypt.genSalt(10);
+    const decipher = await bcrypt.hash(password, encrypt);
 
-        const encrypt = await bcrypt.genSalt(10);
-        const decipher = await bcrypt.hash(password, encrypt)
-        
-        const token = jwt.sign({ decipher }, "code")
+    const token = jwt.sign({ decipher }, "code");
 
-        const user = await userModel.create({
-            userName,
-            email,
-            password: decipher,
-            token,
-            role:Role.USER
-        })
+    const user = await userModel.create({
+      userName,
+      email,
+      password: decipher,
+      token,
+      role: Role.USER,
+    });
 
-        return res.status(HTTP.CREATE).json({
-            message: "User created Successfully",
-            data: user,
-})
-        
-    } catch (error:any) {
-        return res.status(HTTP.BAD).json({
-            message: "Error creating User",
-            data: error.message,
-        })
-    }
-}
-
-export const deleteUser = async (req: Request, res: Response): Promise<Response> => {
-    try {
-        const { userID } = req.params;
-
-        await userModel.findByIdAndDelete(userID)
-      
-        return res.status(HTTP.DELETE).json({
-            message:"User deleted"
-        })
+    return res.status(HTTP.CREATE).json({
+      message: "User created Successfully",
+      data: user,
+    });
   } catch (error: any) {
+    return res.status(HTTP.BAD).json({
+      message: "Error creating User",
+      data: error.message,
+    });
+  }
+};
+
+export const signInUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      const checkPassword = await bcrypt.compare(password, user.password);
+      if (checkPassword) {
+        if (user.verified && user.token === "") {
+          return res.status(HTTP.OK).json({
+            message: "Sign In successfull",
+          });
+        } else {
+          return res.status(HTTP.BAD).json({
+            message: "User is not verified",
+          });
+        }
+      } else {
+        return res.status(HTTP.BAD).json({
+          message: "Incorrect Password",
+        });
+      }
+    } else {
       return res.status(HTTP.BAD).json({
-          message: "Error deleting User",
-          data:error.message
-      })
+        message: "User does not exist",
+      });
+    }
+  } catch (error: any) {
+    return res.status(HTTP.BAD).json({
+      message: "Error creating User",
+      data: error.message,
+    });
+  }
+};
+
+export const deleteUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { userID } = req.params;
+
+    await userModel.findByIdAndDelete(userID);
+
+    return res.status(HTTP.DELETE).json({
+      message: "User deleted",
+    });
+  } catch (error: any) {
+    return res.status(HTTP.BAD).json({
+      message: "Error deleting User",
+      data: error.message,
+    });
   }
 };
 
 export const findAllUser = async (req: Request, res: Response) => {
-    try {
+  try {
+    const findAllUser = await userModel.find();
 
-        const findAllUser = await userModel.find()
-
-        return res.status(HTTP.OK).json({
-          message: "All Users Successfully found",
-          data: findAllUser,
-        });
-    } catch (error:any) {
-        return res.status(HTTP.BAD).json({
-            message: "Error finding all User",
-            data:error.message
-        })
-    }
+    return res.status(HTTP.OK).json({
+      message: "All Users Successfully found",
+      data: findAllUser,
+    });
+  } catch (error: any) {
+    return res.status(HTTP.BAD).json({
+      message: "Error finding all User",
+      data: error.message,
+    });
+  }
 };
+
 export const findOneUser = async (req: Request, res: Response) => {
-    try {
+  try {
+    const { userID } = req.params;
 
-        const { userID } = req.params;
+    const findUser = await userModel.findById(userID);
 
-        const findUser = await userModel.findById(userID)
-
-        return res.status(HTTP.OK).json({
-            message: "User Successfully found",
-            data:findUser,
-        })
-    } catch (error:any) {
-        return res.status(HTTP.BAD).json({
-            message: "Error finding one User",
-            data:error.message
-        })
-    }
+    return res.status(HTTP.OK).json({
+      message: "User Successfully found",
+      data: findUser,
+    });
+  } catch (error: any) {
+    return res.status(HTTP.BAD).json({
+      message: "Error finding one User",
+      data: error.message,
+    });
+  }
 };
