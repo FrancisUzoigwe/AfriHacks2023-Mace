3e8bd28ade71c6b4bd07266b07a5e40f9a1cbaa0
Godswill Udida <udidagodswill7@gmail.com>
2023-12-10 18:23:05 +0100

    udida

--- AfriHack_Be/router/userRouter.ts
+++ AfriHack_Be/router/userRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createStoreOwner, createUser, deleteUser, findAllUser, findOneUser, signInUser, verifyUser } from "../controller/userController";
+import { createUser, deleteUser, findAllUser, findOneUser, signInUser, verifyUser } from "../controller/userController";
 
 
 const router = Router();
 
 router.route("/create-user").post(createUser)
-router.route("/create-owner").post(createStoreOwner)
 router.route("/sign-in-user").post(signInUser)
 router.route("/:token/verify-user").patch(verifyUser)
 router.route("/:userID/find-one-user").get(findOneUser)
